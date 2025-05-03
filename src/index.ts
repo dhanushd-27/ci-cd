3fef420be7d1d8d9333e06683fd2ca312f184e33
dhanushd-27 <dhanushsai2005@gmail.com>
2025-05-03 20:40:13 +0530

    Add Http Server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello world");
+import express from 'express';
+import type { Request, Response } from 'express';
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
