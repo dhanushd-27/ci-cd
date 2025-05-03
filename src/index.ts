dfd4a3eb45efb5fdb64d7e374b31cad98501938f
dhanushd-27 <dhanushsai2005@gmail.com>
2025-05-03 21:19:06 +0530

    Add Workflows

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const PORT = 8080;
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+  res.send('Hello, World! from CI/CD pipeline');
 });
 
 app.listen(PORT, () => {
